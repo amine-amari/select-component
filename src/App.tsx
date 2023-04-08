04d1c897123f5a0ac9b352b3026675b42c548bb3
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-04-08 17:31:09 +0200

    add Select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import { Select } from './select'
+
+const options = [
+  { label: "First", value: 1  },
+  { label: "Second", value: 2  },
+  { label: "Third", value: 3  },
+  { label: "Fourth", value: 4  },
+  { label: "Fifth", value: 5  },
+]
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Select options={options} />
     </div>
   )
 }
