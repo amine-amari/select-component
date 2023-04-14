e5f3348e08fefc125983fb270629f4bfe1bd5a1e
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-04-14 15:01:19 +0200

    add Multiselect logic

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,14 @@ const options: SelectOption[] = [
 ]
 
 function App() {
-  const [value, setValue] = useState<typeof options[0] | undefined>(options[0])
+  const [value1, setValue1] = useState<SelectOption[] >([options[0]])
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0])
 
   return (
     <div className="App">
-      <Select options={options} value={value} onChange={o => setValue(o)}/>
+      <Select multiple options={options} value={value1} onChange={o => setValue1(o)}/>
+      <br />
+      <Select options={options} value={value2} onChange={o => setValue2(o)}/>
     </div>
   )
 }
